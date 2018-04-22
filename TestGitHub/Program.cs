3ee51ba0f9ec535a796b42ea167a15ec012d520e
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2018-04-22 11:54:20 +0300

    Init commit

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -10,6 +10,22 @@ namespace TestGitHub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(new MyClass { MyProperty1 = 100, MyProperty2 = 500 } );
         }
+
+        
+    }
+
+    public class MyClass
+    {
+        public int MyProperty1 { get; set; }
+
+        public int MyProperty2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"P1 - {MyProperty1}  P2 - {MyProperty2}";
+        }
+
     }
 }
